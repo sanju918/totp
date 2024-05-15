67a4e89e259d6890766dea130af78f80eaccb78d
sanju918 <42485083+sanju918@users.noreply.github.com>
2024-05-15 10:32:52 +0530

    added enhancements

--- source/main.py
+++ source/main.py
@@ -12,7 +12,7 @@ def setup_logging():
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         handlers=[
             logging.FileHandler("app.log"),
-            logging.StreamHandler()
+            # logging.StreamHandler()
         ]
     )
 
